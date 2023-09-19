b2d784ebaad5ecc438b2f296847d8bc27b28d8df
lulutza95 <lupusor.10@mail.ru>
2023-09-19 13:09:05 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,18 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String []args){
+    public static void main(String []args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Introduceti un numar ");
         int a = in.nextInt();
-        for(int i = 0;i<=a/2;i++);
-        if(a%2==0){
-            System.out.println("Numarul introdus de la tastatura este par");
+        for (int i = 0; i <= a / 2; i++) {
+            if (a % 2 == 0) {
+                System.out.println("Numarul introdus de la tastatura este par");
+            } else if (a % 2 == 1) {
+                System.out.println("numarul introdus de la tastatura este impar");
+            }
         }
-        else if(a%2==1){
-            System.out.println("numarul introdus de la tastatura este impar");
-        }
+
     }
 }
 
